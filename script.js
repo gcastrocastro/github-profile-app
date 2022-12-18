fd213e8f9cd5cf2b8688b047965e2e01c2016a75
gcastrocastro <gcastrocastro.25@gmail.com>
2022-12-17 17:12:14 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const USER_API = 'https://api.github.com/users/';
+// const USER_REPO = 
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+getUser('gcastrocastro');
+
+async function getUser(username) {
+    const resp = await fetch(USER_API + username);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    getRepos(username);
+    createUserCard(respData);
+}
+
+async function getRepos(username) {
+    const resp = await fetch(USER_API + username + '/repos');
+    const respData = await resp.json();
+
+    // console.log(respData);
+
+    addReposToCard(respData);
+}
+
+function createUserCard (user) {
+
+    const cardHTML = `
+        <div class="card">
+            <div class='top-container'>
+                <div>
+                    <a href="${user.html_url}"><img class="avatar" src="${user.avatar_url}" alt="${user.name}"></a>
+                    <ul class="user-infolist">
+                        <li><i class="fa-solid fa-user-group"></i> ${user.followers}</li>
+                        <li><i class="fa-solid fa-people-group"></i> ${user.following}</li>
+                        <li><i class="fa-solid fa-book-bookmark"></i> ${user.public_repos}</li>
+                    </ul>
+                </div>
+                <div class="user-info">
+                    <a href="${user.html_url}" class="user-link"><h2>${user.name}</h2></a>
+                    <p>${user.bio}</p>
+                </div>
+            </div>
+            <div id="repos">
+            </div>
+        </div>`;
+
+    main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos');
+
+    repos.forEach(repo => {
+        const repoEl = document.createElement('a');
+        repoEl.classList.add('repo');
+
+        repoEl.href = repo.homepage;
+        repoEl.target = '_blank';
+        repoEl.innerText = repo.name;
+
+        reposEl.appendChild(repoEl);
+    });
+
+}
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+
+        search.value = '';
+    }
+});
